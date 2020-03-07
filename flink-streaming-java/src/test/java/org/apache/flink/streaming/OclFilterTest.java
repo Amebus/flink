e5aa4d224df4ea07f9a261047e451d910080da46
Federico MAria Ceruti <federico.ceruti@gmail.com>
2020-03-07 16:08:20 +0100

    nVidia fixes

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/OclFilterTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/OclFilterTest.java
@@ -51,13 +51,19 @@ public class OclFilterTest extends OclContextHelpers.OclTestClass
 	@Test
 	public void OclFilterInteger()
 	{
-		List<IOclTuple> vTuples = GetIntegerTestTuples();
+//		List<IOclTuple> vTuples = GetIntegerTestTuples();
 
 //		List<IOclTuple> vTuples = new ArrayList<>();
 //		vTuples.add(new Tuple1Ocl<>(-1679099059));
 //		vTuples.add(new Tuple1Ocl<>(528136394));
 //		vTuples.add(new Tuple1Ocl<>(-1528862540));
 //		vTuples.add(new Tuple1Ocl<>(-1348335996));
+		
+		List<IOclTuple> vTuples = new ArrayList<>();
+		vTuples.add(new Tuple1Ocl<>(-10));
+		vTuples.add(new Tuple1Ocl<>(20));
+		vTuples.add(new Tuple1Ocl<>(-10));
+		vTuples.add(new Tuple1Ocl<>(-50));
 
 		OclContextHelpers.TupleListInfo vListInfo = new OclContextHelpers.TupleListInfo(vTuples);
 		
@@ -85,7 +91,14 @@ public class OclFilterTest extends OclContextHelpers.OclTestClass
 	public void OclFilterDouble()
 	{
 		
-		List<IOclTuple> vTuples = GetDoubleTestTuples();
+//		List<IOclTuple> vTuples = GetDoubleTestTuples();
+		
+		List<IOclTuple> vTuples = new ArrayList<>();
+		vTuples.add(new Tuple1Ocl<>(-10D));
+		vTuples.add(new Tuple1Ocl<>(20D));
+		vTuples.add(new Tuple1Ocl<>(-10D));
+		vTuples.add(new Tuple1Ocl<>(-50D));
+		
 		OclContextHelpers.TupleListInfo vListInfo = new OclContextHelpers.TupleListInfo(vTuples);
 		
 		Iterable<? extends IOclTuple> vResult = getFilterResult(vTuples, "filterDouble");
@@ -111,7 +124,13 @@ public class OclFilterTest extends OclContextHelpers.OclTestClass
 	@Test
 	public void OclFilterString()
 	{
-		List<IOclTuple> vTuples = GetStringTestTuples();
+//		List<IOclTuple> vTuples = GetStringTestTuples();
+		
+		List<IOclTuple> vTuples = new ArrayList<>();
+		vTuples.add(new Tuple1Ocl<>("-10"));
+		vTuples.add(new Tuple1Ocl<>("20"));
+		vTuples.add(new Tuple1Ocl<>("-10"));
+		vTuples.add(new Tuple1Ocl<>("-50"));
 		OclContextHelpers.TupleListInfo vListInfo = new OclContextHelpers.TupleListInfo(vTuples);
 		
 		Iterable<? extends IOclTuple> vResult = getFilterResult(vTuples, "filterString");
